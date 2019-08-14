a83b88e9ce93bf4a46f4ce069650a919e4b20f94
saiwinchester <rsnarain@gmail.com>
2019-08-14 20:38:35 +0530

    My First COmmit

---
+++ js/index.js
@@ -0,0 +1,48 @@
+// SMOOTH SCROLL
+
+$(function() {
+  $('a[href*=#]:not([href=#])').on('click', function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 900);
+        return false;
+      }
+    }
+  });
+});
+
+// End Smooth Scroll
+
+// Content fade in on scroll
+
+$(document).ready(function() {
+
+  /* Every time the window is scrolled ... */
+  $(window).scroll(function() {
+
+    /* Check the location of each desired element */
+    $('.hideme').each(function(i) {
+
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, fade it in */
+      if (bottom_of_window > bottom_of_object) {
+
+        $(this).animate({
+          'opacity': '1'
+        }, 500);
+
+      }
+
+    });
+
+  });
+
+});
+
+// End Fade in on Scroll
